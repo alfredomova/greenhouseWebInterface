f230090bd6f1bf4294444d882163e7d328fa89d4
Alfredo M <alfredomova@live.com.mx>
2016-12-04 22:08:46 -0600

    .

--- cronjobs/temp_press_save.py
+++ cronjobs/temp_press_save.py
@@ -4,19 +4,31 @@ import json
 from pymongo import MongoClient
 from datetime import datetime
 
+# sudo pip install python-dateutil --upgrade
+from dateutil.relativedelta import *
+
 client = MongoClient("mongodb://10.1.10.100:27017")
 
 db = client['greenhouse']
 
-content = urllib2.urlopen("http://localhost/greenhouse/bmp085.php").read()
+content = urllib2.urlopen("http://localhost/greenhouse/php/bmp085.php").read()
 
 d = json.loads(content)
+
+# save current temp / press
 
 result = db.sensor_bmp085.insert_one({
                 "temperature" : d["temperature"]["C"],
                 "pressure" : d["hpa"],
                 "date" : datetime.now()
-        }
-)
+        })
 
-#print(result.inserted_id)
+# delete older than 3 months
+
+three_mon_rel = relativedelta(months=3)
+
+threeMothsAgo = (datetime.now() - three_mon_rel)
+
+db.sensor_bmp085.remove({
+                "date" : {"$lt" : threeMothsAgo }
+        })
